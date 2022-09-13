246a4f4e6ec06492aba61dd8ae569046dd23c397
Martin Escriba Bab <101904091+MEscriba@users.noreply.github.com>
2022-09-12 22:21:24 -0300

    Create Player.ts

---
+++ TIC_TAC_TOE/MODELS/Player.ts
@@ -0,0 +1,12 @@
+```
+export default class Player {
+  get name() {
+    return this.nikname;
+  }
+
+  set name(name: string) {
+    this.nikname = name;
+  }
+  constructor(private nikname: string) {}
+}
+  ```
